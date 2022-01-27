586e00c72b0774110e92867f750595ddcf399f6a
ngone <ngone@ngones-mbp.lan>
2022-01-27 12:46:27 -0600

    WarnerYiSeck average

--- src/studio1/Average.java
+++ src/studio1/Average.java
@@ -1,10 +1,18 @@
 package studio1;
 
+import support.cse131.ArgsProcessor;
+
 public class Average {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		ArgsProcessor ap = new ArgsProcessor(args);
+		
+		int a = ap.nextInt("Enter the first integer");
+		int b = ap.nextInt("Enter the second integer");
+		double average = (a+b)/2.0; 
+		 
+		System.out.println("Average of " + a + " and " + b + " is " + average +".");
 	}
 
 }
